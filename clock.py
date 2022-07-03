0343ff236e0215f9635bc5faca6de9ac1418e2a1
Achary Anandu Saji <107409467+acharyanandusaji@users.noreply.github.com>
2022-07-03 23:09:10 +0530

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,20 +1,6 @@
 from tkinter import *
 from tkinter.ttk import *
 
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-const firebaseConfig = {
-  apiKey: "AIzaSyDDLa5cEOzSUhT254XbqnqH-y9lPLY6nlI",
-  authDomain: "clock-c8409.firebaseapp.com",
-  projectId: "clock-c8409",
-  storageBucket: "clock-c8409.appspot.com",
-  messagingSenderId: "1021173176267",
-  appId: "1:1021173176267:web:1b7e370bdb64bba48d484e",
-  measurementId: "G-MN83M18X46"
-};
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 from time import strftime
 
 root = Tk()
